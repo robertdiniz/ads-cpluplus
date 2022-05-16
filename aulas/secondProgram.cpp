17f063478e7d8484da955786ba1cbfcf0684f6e3
Robert Diniz <94693689+robertdiniz@users.noreply.github.com>
2022-05-16 15:59:03 -0300

    Aula 1 - Introdução a C++

---
+++ aulas/secondProgram.cpp
@@ -0,0 +1,13 @@
+/* my second program in C++
+with more comments */
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+
+cout << "Hello World!"; // prints Hello World!
+cout << "I'm a C++ program"; // prints I'm C++
+return 0;
+
+}
